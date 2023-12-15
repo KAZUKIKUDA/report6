995ff5699ef11871a8d02411553681218a1a4b2a
Kazuki Kuda <e235704@ie.u-ryukyu.ac.jp>
2023-12-15 13:42:52 +0900

    final

--- app/src/main/java/jp/ac/uryukyu/ie/e235704/matchedNumber.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235704/matchedNumber.java
@@ -5,30 +5,30 @@ public class matchedNumber {
         int i = 0;
         int brow = 0;
         int hit = 0;
-        /*for (int put: putList){
-            for (int random: randomList){
+        for (int put : putList){
+            for (int random : randomList){
                 if (put == random){
                     brow += 1;
                 }
-
             }
-        }*/
-        for (int put1:putList){
+        }
+        for (int put1 : putList){
             if(put1 == randomList.get(i)){
                 hit += 1;
                 i += 1;
             }
         }
+        brow -= hit;
         System.out.println("hit: "+hit);
         System.out.println("brow: "+brow);
     }
     public static void main(String[] args) {
         // ランダムな数字のリストを生成するNumberクラスのインスタンスを作成
         ArrayList<Integer> randomList = new ArrayList<>();
-        randomList.add(0);
-        randomList.add(0);
         randomList.add(1);
         randomList.add(2);
+        randomList.add(0);
+        randomList.add(5);
 
         // プレイヤーが入力した数字のリストを仮定
         ArrayList<Integer> putList = new ArrayList<>();
